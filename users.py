43dd9606981c557bd025c400b85accf163e7eb64
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-15 17:09:04 +0000

    read

--- users.py
+++ users.py
@@ -1,20 +1,23 @@
 from tinydb import TinyDB
+from pprint import pprint
 
 databese = TinyDB('database.json')
 
-databese.truncate()
+pprint(databese.all())
 
-data = dict()
+# databese.truncate()
 
-for i in range(10):
-    data = {}
-    data['user_id'] = int(input('user_id: '))
-    data['username'] = input('username: ')
-    data['first_name'] = input('first_name: ')
-    data['last_ame'] = input('last_name: ')
-    data['birthday'] = input('birthday: ')
-    data['email'] = input('email: ')
-    data['job'] = input('job: ')
-    data['phone'] = input('phono: ')
+# data = dict()
 
-    databese.insert(data)
+# for i in range(10):
+#     data = {}
+#     data['user_id'] = int(input('user_id: '))
+#     data['username'] = input('username: ')
+#     data['first_name'] = input('first_name: ')
+#     data['last_ame'] = input('last_name: ')
+#     data['birthday'] = input('birthday: ')
+#     data['email'] = input('email: ')
+#     data['job'] = input('job: ')
+#     data['phone'] = input('phono: ')
+
+#     databese.insert(data)
